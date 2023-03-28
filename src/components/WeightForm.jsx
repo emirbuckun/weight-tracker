089be2e76c25dccee4f05511a075fc87322db867
Emir Buckun <emirbuckun@hotmail.com>
2023-03-28 09:58:29 +0300

    Add weight form component

--- src/components/WeightForm.jsx
+++ src/components/WeightForm.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
-const WeightForm = () => {
-  return <div>WeightForm</div>;
+const WeightForm = (props) => {
+  const [input, setInput] = useState("");
+  const [id, setId] = useState("");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const getId = () => {
+    const returnId = id;
+    setId(id + 1);
+    return returnId;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onSubmit({
+      id: getId(),
+      date: new Date().getTime(),
+      text: input,
+    });
+    setInput("");
+  };
+
+  return (
+    <form className="weight-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="text"
+        value={input}
+        placeholder="Add weight"
+        className="weight-input"
+        onChange={(e) => setInput(e.target.value)}
+        ref={inputRef}
+      />
+      <button className="add-button"></button>
+    </form>
+  );
 };
 
 export default WeightForm;
